89bea3ef365942febb0da92ffa63a50f52675fcd
TermiusOne <termiusone@gmail.com>
2019-10-05 00:55:42 +0300

    Fixed struct.

--- errors.go
+++ errors.go
@@ -4,39 +4,32 @@ import (
 	"fmt"
 )
 
-type XError interface {
-	Code() int
-	Error() string
-	Wrap(a ...interface{}) XError
-	Wrapf(format string, a ...interface{}) XError
-}
-
-type baseError struct {
+type XError struct {
 	code int
 	msg  string
 }
 
-func New(code int, text string) XError {
-	return &baseError{
+func New(code int, text string) *XError {
+	return &XError{
 		code: code,
 		msg:  text,
 	}
 }
 
-func (e *baseError) Error() string {
+func (e *XError) Error() string {
 	return e.msg
 }
 
-func (e *baseError) Wrap(a ...interface{}) XError {
+func (e *XError) Wrap(a ...interface{}) *XError {
 	e.msg = fmt.Sprintf("%s: %s", e.msg, fmt.Sprint(a...))
 	return e
 }
 
-func (e *baseError) Wrapf(format string, a ...interface{}) XError {
+func (e *XError) Wrapf(format string, a ...interface{}) *XError {
 	e.msg = fmt.Sprintf("%s: %s", e.msg, fmt.Sprintf(format, a...))
 	return e
 }
 
-func (e *baseError) Code() int {
+func (e *XError) Code() int {
 	return e.code
 }
