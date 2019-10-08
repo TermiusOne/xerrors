37a30dc9330fd0e13f595a53ff5b1db9a0d91b21
TermiusOne <termiusone@gmail.com>
2019-10-08 21:39:36 +0300

    Update errors methods.

--- errors.go
+++ errors.go
@@ -2,34 +2,41 @@ package xerrors
 
 import (
 	"fmt"
+	"strconv"
 )
 
 type XError struct {
-	code int
-	msg  string
+	logic bool
+	code  int64
+	msg   string
 }
 
-func New(code int, text string) *XError {
+func New(code int64, text string, logic bool) *XError {
 	return &XError{
-		code: code,
-		msg:  text,
+		code:  code,
+		msg:   text,
+		logic: logic,
 	}
 }
 
-func (e *XError) Error() string {
-	return e.msg
-}
-
-func (e *XError) Wrap(a ...interface{}) *XError {
-	e.msg = fmt.Sprintf("%s: %s", e.msg, fmt.Sprint(a...))
-	return e
-}
-
-func (e *XError) Wrapf(format string, a ...interface{}) *XError {
-	e.msg = fmt.Sprintf("%s: %s", e.msg, fmt.Sprintf(format, a...))
-	return e
-}
-
-func (e *XError) Code() int {
+func (e XError) Code() int64 {
 	return e.code
+}
+
+func (e XError) IsLogic() bool {
+	return e.logic
+}
+
+func (e *XError) WithError(err error) *XError {
+	e.msg += ": " + err.Error()
+	return e
+}
+
+func (e *XError) WithDetails(format string, args ...interface{}) *XError {
+	e.msg += ": " + fmt.Sprintf(format, args...)
+	return e
+}
+
+func (e *XError) Error() string {
+	return "code: " + strconv.FormatInt(e.code, 10) + ", error: " + e.msg
 }
--- errors_test.go
+++ errors_test.go
@@ -1,26 +1,33 @@
 package xerrors
 
 import (
+	"errors"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestBaseError_Code(t *testing.T) {
-	var code = 10
+	var code int64 = 10
 
-	someErr := New(code, "1")
+	someErr := New(code, "1", true)
 
 	assert.Equal(t, someErr.Code(), code)
 }
 
-func TestBaseError_Wrap(t *testing.T) {
-	err := New(1, "some message 1").Wrap("some message 2")
+func TestXError_IsLogic(t *testing.T) {
+	someErr := New(10, "1", true)
 
-	assert.Equal(t, err.Error(), "some message 1: some message 2")
+	assert.Equal(t, someErr.IsLogic(), true)
 }
 
-func TestBaseError_Wrapf(t *testing.T) {
-	err := New(1, "some message 1").Wrapf("some message %v", 2)
+func TestBaseError_WithError(t *testing.T) {
+	err := New(1, "some message 1", true).WithError(errors.New("some message 2"))
 
-	assert.Equal(t, err.Error(), "some message 1: some message 2")
+	assert.Equal(t, err.Error(), "code: 1, error: some message 1: some message 2")
+}
+
+func TestBaseError_WithDetails(t *testing.T) {
+	err := New(2, "some message 1", false).WithDetails("some message %d", 2)
+
+	assert.Equal(t, err.Error(), "code: 2, error: some message 1: some message 2")
 }
