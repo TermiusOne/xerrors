53d5bd2bfc087ac80f39acb749285da93135cebd
TermiusOne <termiusone@gmail.com>
2019-10-12 17:35:12 +0300

    Added interface

--- errors.go
+++ errors.go
@@ -5,38 +5,46 @@ import (
 	"strconv"
 )
 
-type XError struct {
+type Error interface {
+	Code() int64
+	IsLogic() bool
+	WithError(err error) Error
+	WithDetails(format string, args ...interface{}) Error
+	Error() string
+}
+
+type xError struct {
 	logic bool
 	code  int64
 	msg   string
 }
 
-func New(code int64, text string, logic bool) *XError {
-	return &XError{
+func New(code int64, text string, logic bool) Error {
+	return &xError{
 		code:  code,
 		msg:   text,
 		logic: logic,
 	}
 }
 
-func (e XError) Code() int64 {
+func (e xError) Code() int64 {
 	return e.code
 }
 
-func (e XError) IsLogic() bool {
+func (e xError) IsLogic() bool {
 	return e.logic
 }
 
-func (e *XError) WithError(err error) *XError {
+func (e xError) WithError(err error) Error {
 	e.msg += ": " + err.Error()
 	return e
 }
 
-func (e *XError) WithDetails(format string, args ...interface{}) *XError {
+func (e xError) WithDetails(format string, args ...interface{}) Error {
 	e.msg += ": " + fmt.Sprintf(format, args...)
 	return e
 }
 
-func (e *XError) Error() string {
+func (e xError) Error() string {
 	return "code: " + strconv.FormatInt(e.code, 10) + ", error: " + e.msg
 }
